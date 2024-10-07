efc2e70afe23f99931aeac6ffeb307ef84aedfea
hrodriguezg5 <hrodriguezg5@miumg.edu.gt>
2024-10-06 20:09:05 -0600

    Agregando modulo de Usuarios y Permisos

--- public/js/modules/login.js
+++ public/js/modules/login.js
@@ -1,6 +1,6 @@
 // Alternar visibilidad de contraseña
 const togglePassword = document.querySelector("#togglePassword");
-const passwordField = document.querySelector("#floatingPassword");
+const passwordField = document.querySelector("#loginPassword");
 
 togglePassword.addEventListener("click", function () {
     // Alternar el atributo de tipo
@@ -11,3 +11,31 @@ togglePassword.addEventListener("click", function () {
     this.querySelector('i').classList.toggle("bi-eye");
     this.querySelector('i').classList.toggle("bi-eye-slash");
 });
+
+document.getElementById('loginForm').onsubmit = async function (e) {
+    e.preventDefault();
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+    const errorMessage = document.getElementById('errorMessage');
+
+    const response = await fetch('http://localhost/clinica/login/ingresar', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password })
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        if (data.success) {
+            localStorage.setItem('token', data.token); // Guardar token en localStorage
+            window.location.href = 'http://localhost/clinica/paciente'; // Redirigir al dashboard
+        } else {
+            errorMessage.textContent = data.message;
+        }
+    } else {
+        const error = await response.json();
+        alert(error.message);
+    }
+};
