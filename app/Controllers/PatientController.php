dd69c3289da779f1bb975b28fbd17f338b86e314
Jose Monroy <jyatm4@miumg.edu.gt>
2024-10-12 16:24:04 -0600

    Creacion de api pacientes completa

--- app/Controllers/PatientController.php
+++ app/Controllers/PatientController.php
@@ -13,31 +13,6 @@ class PatientController extends Controllers {
         $response = $this->getUserAndModules($data);
         $this->jsonResponse($response);
     }
-    
-    public function customer() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (!$this->authMiddleware->validateToken()) return;
-            $input = json_decode(file_get_contents("php://input"), true);
-            $id = filter_var($input['id'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) ?? '';
-            $customer = $this->model->getCustomer(['id' => $id]);
-
-            if ($customer) {
-                $response = [
-                    'id' => $customer->id,
-                    'first_name' => $customer->first_name,
-                    'last_name' => $customer->last_name,
-                    'email' => $customer->email,
-                    'phone_number' => $customer->phone_number,
-                    'address' => $customer->address,
-                    'product' => $customer->product,
-                    'product_quantity' => $customer->product_quantity,
-                    'hour' => $customer->reservation_hour,
-                    'date' => $customer->reservation_date
-                ];
-                $this->jsonResponse($response);
-            }
-        }
-    }
 
     public function show() {
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
@@ -98,48 +73,89 @@ class PatientController extends Controllers {
 
     public function update() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (!$this->authMiddleware->validateToken()) return;
-            $userId = $_SESSION['user_id'] ?? 1;
-
-            $data = [
-                "id" => filter_input(INPUT_POST, 'id', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                "first_name" => filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_SPECIAL_CHARS),
-                "last_name" => filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_SPECIAL_CHARS),
-                "email" => filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL),
-                "phone_number" => filter_input(INPUT_POST, 'phone_number', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                "product_id" => filter_input(INPUT_POST, 'product_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                "product_quantity" => filter_input(INPUT_POST, 'product_quantity', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                "address" => filter_input(INPUT_POST, 'address', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                "date" => filter_input(INPUT_POST, 'date', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                "hour_id" => filter_input(INPUT_POST, 'hour_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                "user_id" => $userId
-            ];
-
-            if ($this->model->updateReservation($data)) {
-                $this->jsonResponse(["success" => true]);
-            } else {
-                $this->jsonResponse(["success" => false]);
+            if (!$this->authMiddleware->validateToken()) {
+                $this->jsonResponse(["success" => false, "message" => "Token de autenticación inválido."]);
+                return;
             }
+            
+            // Obtener el contenido de la solicitud y decodificar el JSON
+            $json = file_get_contents('php://input');
+            $decodedData = json_decode($json, true); // Decodifica el JSON en un array asociativo
+    
+            $data = [
+                "id" => isset($decodedData['id']) ? filter_var($decodedData['id'], FILTER_SANITIZE_NUMBER_INT) : null,
+                "first_name" => isset($decodedData['first_name']) ? filter_var($decodedData['first_name'], FILTER_SANITIZE_SPECIAL_CHARS) : null,
+                "last_name" => isset($decodedData['last_name']) ? filter_var($decodedData['last_name'], FILTER_SANITIZE_SPECIAL_CHARS) : null,
+                "birth_date" => isset($decodedData['birth_date']) ? filter_var($decodedData['birth_date'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : null,
+                "gender" => isset($decodedData['gender']) ? filter_var($decodedData['gender'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : null,
+                "address" => isset($decodedData['address']) ? filter_var($decodedData['address'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : null,
+                "phone" => isset($decodedData['phone']) ? filter_var($decodedData['phone'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : null,
+                "email" => isset($decodedData['email']) ? filter_var($decodedData['email'], FILTER_SANITIZE_EMAIL) : null,
+                "updated_by" => isset($decodedData['updated_by']) ? filter_var($decodedData['updated_by'], FILTER_SANITIZE_EMAIL) : null
+            ];
+            
+            if ($this->model->updatePatient($data)) {
+                $this->jsonResponse(["success" => true, "message" => "Reserva actualizada exitosamente."]);
+            } else {
+                $this->jsonResponse(["success" => false, "message" => "Error al actualizar la reserva."]);
+            }
+        } else {
+            
+            $this->jsonResponse(["success" => false, "message" => "Método no permitido."]);
         }
     }
+    
 
     public function delete() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (!$this->authMiddleware->validateToken()) return;
             $userId = $_SESSION['user_id'] ?? 1;
-
+    
+            // Obtener el contenido de la solicitud y decodificar el JSON
+            $json = file_get_contents('php://input');
+            $decodedData = json_decode($json, true); // Decodifica el JSON en un array asociativo
+    
             $data = [
-                "id" => filter_input(INPUT_POST, 'id', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                "user_id" => $userId
+                "id" => isset($decodedData['id']) ? filter_var($decodedData['id'], FILTER_SANITIZE_NUMBER_INT) : null, 
+                "deleted_by" => isset($decodedData['deleted_by']) ? filter_var($decodedData['deleted_by'], FILTER_SANITIZE_NUMBER_INT) : null 
             ];
-
-            if ($this->model->deleteReservation($data)) {
+    
+            // Validar que el ID no sea nulo
+            if ($data['id'] === null) {
+                $this->jsonResponse(["success" => false, "message" => "Falta el ID del recurso."]);
+                return;
+            }
+    
+            if ($this->model->deletePatient($data)) {
                 $this->jsonResponse(["success" => true]);
             } else {
                 $this->jsonResponse(["success" => false]);
             }
         }
     }
+
+    public function search() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (!$this->authMiddleware->validateToken()) return;
+    
+            $json = file_get_contents('php://input');
+            $decodedData = json_decode($json, true); 
+    
+            $name = isset($decodedData['name']) ? filter_var($decodedData['name'], FILTER_SANITIZE_SPECIAL_CHARS) : null;
+            var_dump($name);
+            if ($name === null) {
+                $this->jsonResponse(["success" => false, "message" => "Falta el nombre del paciente."]);
+                return;
+            }
+    
+            if ($this->model->searchPatients($name)) {
+                $this->jsonResponse(["success" => true]);
+            } else {
+                $this->jsonResponse(["success" => false]);
+            }
+        }
+    }
+    
 }
 
 ?>
